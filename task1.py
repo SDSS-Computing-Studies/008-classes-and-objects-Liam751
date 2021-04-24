3a87b4ecf0f1af88fcf42dffdf38b0f24522104a
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-04-23 20:43:06 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,8 +7,7 @@ str name
 str studentNumber
 int grade
 list courses - to corresepond with course names
-list grades - to correspond with grades
-
+list grade - to correspond with grades
 It should have the following methods:
 average()       - determines the mathematical average of all course grades
 getHonorRoll()  - determines the average of the top 5 courses if there are at least 5 courses.
@@ -23,31 +22,61 @@ constructor     - should require the student name, studentNumber and grade (in t
 """
 
 class student:
+    name = ""
+    code = ""
+    grade = int()
+    courses = []
+    grades = []
 
-    # properties should be listed first
+    def getCourses(self, lis):
+        self.courses = lis
 
-    def __init__(): # You will need to create your own input parameters for all methods
-        pass
+    def getGrades(self, *arg):
+        gr = []
+        for x in range(len(arg)):
+            gr.append(arg[x])
+        self.grades = gr
 
-    def __del__():
+    def __init__(self, name, code, grade): 
+        self.name = name
+        self.code = code
+        self.grade = grade
+
+    def __del__(self):
         pass
 
     def average(self):
-        pass
+        av = 0
+        for x in range(len(self.grades)):
+            av += self.grades[x]
+        return round(av / len(self.grades), 1)
+
+    def getHonorRoll(self):
+        lis = self.grades
+        lis.sort(reverse = True)
+        hr = 0
+        for x in range(5):
+            hr += lis[x]
+        ans = hr / 5
+        if ans >= 86:
+            return True
+        else:
+            return False
+    
+    def showCourses(self):
+        print(self.courses)
+
+    def showGrade(self, x):
+        print(self.courses[x] + " " + self.grades[x])
+
 
 def main():
-    # This contains test data that will be used by the autograder.
-    # do not modify this function
-
     st1 = student("Anita Bath","91334",11)
     st1.getCourses( ["English","Math","PE","Computers","History","Biology","Japanese"] )
-    st1.getGrades( [91, 94, 87, 99, 82, 100, 73])
+    st1.getGrades( 91, 94, 87, 99, 82, 100, 73)
 
     st2 = student("Joe Lunchbox","12346", 11)
-    st2.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
-    st2.getGrades( [71, 98, 93, 95, 68, 81, 71])
-
-
-
+    st1.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
+    st1.getGrades( 71, 98, 93, 95, 68, 81, 71)
 
 main()
